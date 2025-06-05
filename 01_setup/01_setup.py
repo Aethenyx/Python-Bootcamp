75114f88d867caedbe3550ec1ba89a135f578205
Z <juber.kudchi@gmail.com>
2025-06-05 20:20:27 +0530

    Initial commit

---
+++ 01_setup/01_setup.py
@@ -0,0 +1,6 @@
+# In this session of proggramming I just added the etensions that i must work with for the support of python in the vs code. The extensions installed are:
+# Python (from microsoft)
+# Pylance (from miscrosoft)
+# Python debugger (from microsoft)
+# code runner (from Jun Han)
+# syntax highlighter (from Evgeniy Peshkov)
