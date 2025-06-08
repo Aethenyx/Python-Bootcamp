8c749d513e9668094ac02b4976fb5dd00bcbeff1
Z <juber.kudchi@gmail.com>
2025-06-08 13:53:48 +0530

    added 03_variables

---
+++ 03_variables/01_basics.py
@@ -0,0 +1,11 @@
+# In Python, variables are used to store data that can be used and manipulated throughout a program. A variable is created the moment you assign a value to it using the assignment operator(=).
+
+age = 13
+name = "Aethenyx"
+cgpa = 9.9
+
+# Rules of defining a variable in python:
+# Variable names must start with letter (a-z, A-A) or an underscore(_)
+#  They can contain letters, number and underscores.
+# Variables names are case sensitive (age and Age are different).
+# Avoid using python keywords (e.g, if, for, while) as variable names. 
---
+++ 03_variables/02_data_types.py
@@ -0,0 +1,25 @@
+# Python has several built-in data types:
+#     Integers (int): whole numbers (e.g., 10, -6, etc)
+#     Floats (float): Decimal numbers (e.g., 3.14, -0.11, etc).
+#     Strings (str): Text data enclosed in quotes (e.g., "Hello", "Aethenyx").
+#     Booleans(bool): Represents True or False
+#     Lists: Ordered mutable collections (e.g., [1, 2, 3, 4, 5]).
+#     Tuples: Ordered, immutable collections (e.g., (1, 2, 3, 4, 5)).
+#     Sets: Unordered collections of unique elements (e.g., {1, 2, 3, 4, 5}).
+#     Dictionaries: key-value pairs (e.g., {"name": "Aethenyx", "age": "13"})
+
+age = 13
+print(age)
+print(type(age))
+
+cgpa = 9.8
+print(cgpa)
+print(type(cgpa))
+
+name = "Aethenyx"
+print(name)
+print(type(name))
+
+is_completed = True # can also be False
+print(is_completed)
+print(type(is_completed))
