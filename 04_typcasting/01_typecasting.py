0907c67c9ec235be21d0d86c72a6f10c82d78ab2
Z <juber.kudchi@gmail.com>
2025-06-12 17:44:52 +0530

    added typecasting

---
+++ 04_typcasting/01_typecasting.py
@@ -0,0 +1,13 @@
+a = 17 # integer
+print(type(a))
+
+b = str(a) # converted to string
+print(type(b))
+
+pi = 3.14 # float 
+print(pi)
+print(type(pi))
+
+pi_int = int(pi)
+print(pi_int) # integer
+print(type(pi))
