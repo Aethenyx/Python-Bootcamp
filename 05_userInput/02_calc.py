2cdf7cc2378a5058f9bfe249bc2c837eb39a0796
Z <juber.kudchi@gmail.com>
2025-06-14 11:05:29 +0530

    Create 02_calc.py

---
+++ 05_userInput/02_calc.py
@@ -0,0 +1,12 @@
+# making a "very basic" calculator
+num1 = int(input("Enter the number: "))
+num2 = int(input("Enter the number: "))
+
+sum = num1 + num2
+difference = num1 - num2
+product = num1 * num2
+quotient = num1 / num2
+print(f"sum: {sum}")
+print(f"difference: {difference}")
+print(f"product: {product}")
+print(f"quotient: {quotient}")
