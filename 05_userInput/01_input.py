88fa3d81099ff886383bc691d0bea16505f1b2b5
Z <juber.kudchi@gmail.com>
2025-06-14 11:04:15 +0530

    Create 01_input.py

---
+++ 05_userInput/01_input.py
@@ -0,0 +1,13 @@
+# Input can be taken from the users by using the "input()" function
+
+user = input("Enter yopur name: ")
+print(user)
+
+string = str(input("Enter your name: ")) # takes the input in the form of string 
+print(string)
+
+integer = int(input("Enter the number: ")) # takes input in the form of integer
+print(integer)
+
+floatingInteger = float(input("Enter the cgpa: ")) # takes input in the form of float
+print(floatingInteger)
