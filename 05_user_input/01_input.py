c94da981bf27b71f7b3d54c1d5b9a1292222b327
Aethenyx <aethenyx@gmail.com>
2025-08-30 17:18:10 +0530

    added more files

---
+++ 05_user_input/01_input.py
@@ -0,0 +1,21 @@
+# Basic syntax
+a = input("Enter a number: ")
+print(a)
+
+name = input("Enter your name: ")
+print(name)
+
+
+# some rules to remeber
+
+input1 = str(input("Enter a string: ")) # If you enter any other casting function except for a string, you will get an error
+print(input1)
+
+input2 = int(input("Enter a number: ")) # If you enter any other casting function except for an integer, you will get an error
+print(input2)
+
+input3 = bool(input("Enter True or False: ")) # If you enter any other casting function except for a boolean, you will get an error
+print(input3)
+
+input4 = float(input("Enter a floating integer: ")) # If you enter any other casting function exceot for a floating integer, you will get an error
+print(input4)
